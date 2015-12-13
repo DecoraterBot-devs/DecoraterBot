485a3eb04f5b3ee7cdecac8c6e83f49e5b6b66c9
AraHaan <seandhunt_7@yahoo.com>
2015-12-13 03:20:27 -0600

    updated

--- DecoraterBot.py
+++ DecoraterBot.py
@@ -6,24 +6,14 @@ import sys
 import os.path
 import discord
 
-PATH='.\login.ini'
 where()
 os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(os.path.dirname(sys.executable), "cacert.pem")
 
 client = discord.Client()
 DecoraterBotCore.Core.changeWindowTitle()
-
-if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-    import ConfigParser
-    config = ConfigParser.ConfigParser()
-    config.readfp(open(PATH))
-    discord_user_email = config.get("login", "email")
-    discord_user_password = config.get("login", "password")
-    client.login(discord_user_email, discord_user_password)
-else:
-    discord_user_email = 'email'
-    discord_user_password = 'password'
-    client.login(discord_user_email, discord_user_password)
+DecoraterBotCore.Core.changeWindowSize()
+DecoraterBotCore.Logininfo.login_info(client)
+DecoraterBotCore.OnLogin.variable()
 
 @client.event
 def on_message(message):
@@ -32,14 +22,19 @@ def on_message(message):
 DecoraterBotCore.Core.on_error(on_message)
 
 @client.event
+def on_message_delete(message):
+    DecoraterBotCore.Core.deletemessage(message)
+
+@client.event
+def on_message_edit(before, after):
+    DecoraterBotCore.Core.editmessage(before, after)
+
+@client.event
+def on_channel_create(channel):
+    DecoraterBotCore.Channels.data(client, channel)
+
+@client.event
 def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    client.send_message(discord.Object(id='93740277918871552'), "**DecoraterBot Status: Online**")
-#    client.send_message(discord.Object(id='110374132432011264'), "**DecoraterBot Status: Online**")
-    client.send_message(discord.Object(id='118098998744580098'), "**DecoraterBot Status: Online**")
-    client.send_message(discord.Object(id='81392063312044032'), "**DecoraterBot Status: Online**")
+    DecoraterBotCore.OnLogin.on_login(client)
 
 client.run()

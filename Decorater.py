cf1b1f585a9c9772235ae9b444b9975b8d915a0f
AraHaan <seandhunt_7@yahoo.com>
2016-02-16 00:48:07 -0600

    Fixed Sadly is only Supported in python 3.5+ now.

--- Decorater.py
+++ Decorater.py
@@ -4,6 +4,7 @@ import DecoraterCore
 import sys
 import os.path
 import ctypes
+import asyncio
 
 PATH='.\login.ini'
 
@@ -11,13 +12,11 @@ client = discord.Client()
 DecoraterCore.Core.changeWindowTitle()
 
 @client.event
-@asyncio.coroutin
-def on_message(message):
+async def on_message(message):
     DecoraterCore.Core.commands(client, message)
 
 @client.event
-@asyncio.coroutin
-def on_ready():
+async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
@@ -29,10 +28,8 @@ if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
     config.readfp(open(PATH))
     discord_user_email = config.get("login2", "email")
     discord_user_password = config.get("login2", "password")
-    discord_user_id = config.get("login2", "userid")
     client.run(discord_user_email, discord_user_password)
 else:
     discord_user_email = 'email'
     discord_user_password = 'password'
-    discord_user_id = 'user_id'
     client.run(discord_user_email, discord_user_password)
--- DecoraterBot.py
+++ DecoraterBot.py
@@ -12,30 +12,23 @@ DecoraterBotCore.Core.changeWindowSize()
 DecoraterBotCore.OnLogin.variable()
 
 @client.event
-@asyncio.coroutine
-def on_message(message):
-    DecoraterBotCore.Core.commands(client, message)
-
-DecoraterBotCore.Core.on_error(on_message)
+async def on_message(message):
+    await DecoraterBotCore.Core.commands(client, message)
 
 @client.event
-@asyncio.coroutine
-def on_message_delete(message):
+async def on_message_delete(message):
     DecoraterBotCore.Core.deletemessage(message)
 
 @client.event
-@asyncio.coroutine
-def on_message_edit(before, after):
+async def on_message_edit(before, after):
     DecoraterBotCore.Core.editmessage(before, after)
 
 @client.event
-@asyncio.coroutine
-def on_channel_create(channel):
-    DecoraterBotCore.Channels.data(client, channel)
+async def on_channel_create(channel):
+    await DecoraterBotCore.Channels.data(client, channel)
 
 @client.event
-@asyncio.coroutine
-def on_ready():
-    DecoraterBotCore.OnLogin.on_login(client)
+async def on_ready():
+    await DecoraterBotCore.OnLogin.on_login(client)
 
 DecoraterBotCore.Logininfo.login_info(client)

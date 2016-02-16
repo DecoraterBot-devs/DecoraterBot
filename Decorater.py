45a35c59e7889b0178bfb5aa15652bfcc5d19ed5
AraHaan <seandhunt_7@yahoo.com>
2016-02-15 22:48:47 -0600

    Async Bot Code Currently

--- Decorater.py
+++ Decorater.py
@@ -4,15 +4,24 @@ import DecoraterCore
 import sys
 import os.path
 import ctypes
-from requests.certs import where
-
 
 PATH='.\login.ini'
-where()
-os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(os.path.dirname(sys.executable), "cacert.pem")
 
 client = discord.Client()
 DecoraterCore.Core.changeWindowTitle()
+
+@client.event
+@asyncio.coroutin
+def on_message(message):
+    DecoraterCore.Core.commands(client, message)
+
+@client.event
+@asyncio.coroutin
+def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
 if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
     import ConfigParser
@@ -21,22 +30,9 @@ if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
     discord_user_email = config.get("login2", "email")
     discord_user_password = config.get("login2", "password")
     discord_user_id = config.get("login2", "userid")
-    client.login(discord_user_email, discord_user_password)
+    client.run(discord_user_email, discord_user_password)
 else:
     discord_user_email = 'email'
     discord_user_password = 'password'
     discord_user_id = 'user_id'
-    client.login(discord_user_email, discord_user_password)
-
-@client.event
-def on_message(message):
-    DecoraterCore.Core.commands(client, message)
-
-@client.event
-def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-client.run()
+    client.run(discord_user_email, discord_user_password)
--- DecoraterBot.py
+++ DecoraterBot.py
@@ -1,40 +1,41 @@
 #coding=utf-8
 import os
 import DecoraterBotCore
-from requests.certs import where
 import sys
 import os.path
 import discord
-
-where()
-os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(os.path.dirname(sys.executable), "cacert.pem")
+import asyncio
 
 client = discord.Client()
 DecoraterBotCore.Core.changeWindowTitle()
 DecoraterBotCore.Core.changeWindowSize()
-DecoraterBotCore.Logininfo.login_info(client)
 DecoraterBotCore.OnLogin.variable()
 
 @client.event
+@asyncio.coroutine
 def on_message(message):
     DecoraterBotCore.Core.commands(client, message)
 
 DecoraterBotCore.Core.on_error(on_message)
 
 @client.event
+@asyncio.coroutine
 def on_message_delete(message):
     DecoraterBotCore.Core.deletemessage(message)
 
 @client.event
+@asyncio.coroutine
 def on_message_edit(before, after):
     DecoraterBotCore.Core.editmessage(before, after)
 
 @client.event
+@asyncio.coroutine
 def on_channel_create(channel):
     DecoraterBotCore.Channels.data(client, channel)
 
 @client.event
+@asyncio.coroutine
 def on_ready():
     DecoraterBotCore.OnLogin.on_login(client)
 
-client.run()
+DecoraterBotCore.Logininfo.login_info(client)

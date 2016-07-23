c36bc91cc84106f0c9d15e9d6e5e1b6fa09f88b8
AraHaan <seandhunt_7@yahoo.com>
2016-07-23 05:27:43 -0500

    Latest Fixes to Discord.py from @Rapptz (Danny)'s Repo

--- resources/Dependencies/discord/member.py
+++ resources/Dependencies/discord/member.py
@@ -29,6 +29,7 @@ from .game import Game
 from . import utils
 from .enums import Status, ChannelType
 from .colour import Colour
+import copy
 
 
 class VoiceState:
@@ -64,10 +65,7 @@ class VoiceState:
         self.is_afk = kwargs.get('suppress', False)
         self.mute = kwargs.get('mute', False)
         self.deaf = kwargs.get('deaf', False)
-        self._handle_voice_channel(kwargs.get('voice_channel'), kwargs.get('user_id'))
-
-    def _handle_voice_channel(self, voice_channel, user_id):
-        self.voice_channel = voice_channel
+        self.voice_channel = kwargs.get('voice_channel')
 
 def flatten_voice_states(cls):
     for attr in VoiceState.__slots__:
@@ -142,23 +140,10 @@ class Member(User):
 
         self.voice.voice_channel = vc
 
-    def _handle_voice_channel(self, voice_channel, user_id):
-        old_channel = getattr(self, 'voice_channel', None)
-        self.voice_channel = kwargs.get('voice_channel')
-
-        if old_channel is None and self.voice_channel is not None:
-            # we joined a channel
-            self.voice_channel.voice_members.append(self)
-        elif old_channel is not None:
-            try:
-                # we either left a channel or we switched channels
-                old_channel.voice_members.remove(self)
-            except ValueError:
-                pass
-            finally:
-                # we switched channels
-                if self.voice_channel is not None:
-                    self.voice_channel.voice_members.append(self)
+    def _copy(self):
+        ret = copy.copy(self)
+        ret.voice = copy.copy(self.voice)
+        return ret
 
     @property
     def colour(self):

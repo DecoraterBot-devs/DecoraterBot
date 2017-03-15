3674cfcbb3fa2e54aa63aa3ac7f65d90fee9c05a
AraHaan <seandhunt_7@yahoo.com>
2017-03-15 16:20:16 -0500

    Update DecoraterBot64.bat

--- DecoraterBot64.bat
+++ DecoraterBot64.bat
@@ -1,3 +1,3 @@
 @echo off
-"%~dp0\resources\python35x64\win32\python.exe" "%~dp0DecoraterBot.py" 2>resources\Logs\unhandled_tracebacks.txt
+"%SystemDrive%\Python36x64\python.exe" "%~dp0DecoraterBot.py" 2>resources\Logs\unhandled_tracebacks.txt
 pause

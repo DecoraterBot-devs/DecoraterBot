3228496ef265c762d6a94633253583e74e374454
AraHaan <seandhunt_7@yahoo.com>
2017-05-06 22:48:05 -0500

    Update BotPMError.py

--- resources/Dependencies/DecoraterBotCore/BotPMError.py
+++ resources/Dependencies/DecoraterBotCore/BotPMError.py
@@ -11,7 +11,18 @@ Core to DecoraterBot
 """
 import discord
 
-__all__ = ['BotPMError']
+__all__ = ['BotPMError', 'construct_reply']
+
+
+def construct_reply(message):
+    """
+    Constructs an bot reply.
+    """
+    svr_name = message.server.name
+    cnl_name = message.channel.name
+    msginfo = 'Missing the Send Message Permissions in the ' \
+              '{0} server on the {1} channel.'
+    return msginfo.format(svr_name, cnl_name)
 
 
 class BotPMError:
@@ -21,35 +32,20 @@ class BotPMError:
     def __init__(self, bot):
         self.bot = bot
 
-    def construct_reply(self, message):
-        """Constructs an bot reply."""
-        svr_name = message.channel.server.name
-        cnl_name = message.channel.name
-        msginfo = 'Missing the Send Message Permssions in the ' \
-                  '{0} server on the {1} channel.'
-        unabletosendmessageerror = msginfo.format(svr_name, cnl_name)
-        return unabletosendmessageerror
-
     async def resolve_send_message_error(self, ctx):
         """
-        Relolves Errors when Sending messages.
-        :param ctx: Merssage Context.
-        :return: Nothing.
+        Resolves errors when sending messages.
         """
         await self.resolve_send_message_error_old(
             ctx.message)
 
     async def resolve_send_message_error_old(self, message):
         """
-        Relolves Errors when Sending messages.
-        :param message: Merssage.
-        :return: Nothing.
+        Resolves errors when sending messages.
         """
-        unabletosendmessageerror = self.construct_reply(
-            message)
         try:
             await self.bot.send_message(
                 message.author,
-                content=unabletosendmessageerror)
+                content=construct_reply(message))
         except discord.errors.Forbidden:
             return

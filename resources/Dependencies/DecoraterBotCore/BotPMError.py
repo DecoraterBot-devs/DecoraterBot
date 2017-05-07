499efb45d4cb998a3daaca0bf181ad6a56964e97
AraHaan <seandhunt_7@yahoo.com>
2017-05-06 20:37:38 -0500

    Update BotPMError.py

--- resources/Dependencies/DecoraterBotCore/BotPMError.py
+++ resources/Dependencies/DecoraterBotCore/BotPMError.py
@@ -11,44 +11,45 @@ Core to DecoraterBot
 """
 import discord
 
-__all__ = ['resolve_send_message_error', 'resolve_send_message_error_old']
+__all__ = ['BotPMError']
 
 
-def construct_reply(message):
-    """Constructs an bot reply."""
-    svr_name = message.channel.server.name
-    cnl_name = message.channel.name
-    msginfo = 'Missing the Send Message Permssions in the ' \
-              '{0} server on the {1} channel.'
-    unabletosendmessageerror = msginfo.format(svr_name, cnl_name)
-    return unabletosendmessageerror
-
-
-async def resolve_send_message_error(bot, ctx):
+class BotPMError:
     """
-    Relolves Errors when Sending messages.
-    :param bot: Discord Client.
-    :param ctx: Merssage Context.
-    :return: Nothing.
+    Class for PMing bot errors.
     """
-    unabletosendmessageerror = construct_reply(ctx.message)
-    try:
-        await bot.send_message(ctx.message.author,
-                               content=unabletosendmessageerror)
-    except discord.errors.Forbidden:
-        return
+    def __init__(self, bot):
+        self.bot = bot
 
+    def construct_reply(self, message):
+        """Constructs an bot reply."""
+        svr_name = message.channel.server.name
+        cnl_name = message.channel.name
+        msginfo = 'Missing the Send Message Permssions in the ' \
+                  '{0} server on the {1} channel.'
+        unabletosendmessageerror = msginfo.format(svr_name, cnl_name)
+        return unabletosendmessageerror
 
-async def resolve_send_message_error_old(bot, message):
-    """
-    Relolves Errors when Sending messages.
-    :param bot: Discord Client.
-    :param message: Merssage.
-    :return: Nothing.
-    """
-    unabletosendmessageerror = construct_reply(message)
-    try:
-        await bot.send_message(message.author,
-                               content=unabletosendmessageerror)
-    except discord.errors.Forbidden:
-        return
+    async def resolve_send_message_error(self, ctx):
+        """
+        Relolves Errors when Sending messages.
+        :param ctx: Merssage Context.
+        :return: Nothing.
+        """
+        await self.resolve_send_message_error_old(
+            ctx.message)
+
+    async def resolve_send_message_error_old(self, message):
+        """
+        Relolves Errors when Sending messages.
+        :param message: Merssage.
+        :return: Nothing.
+        """
+        unabletosendmessageerror = self.construct_reply(
+            message)
+        try:
+            await bot.send_message(
+                message.author,
+                content=unabletosendmessageerror)
+        except discord.errors.Forbidden:
+            return

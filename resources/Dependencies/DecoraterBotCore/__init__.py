2e1343ed10bf94d75e0a3baa7dbbd89b51e4781a
AraHaan <seandhunt_7@yahoo.com>
2018-04-24 15:04:55 -0400

    Changes to things.

--- resources/Dependencies/DecoraterBotCore/__init__.py
+++ resources/Dependencies/DecoraterBotCore/__init__.py
@@ -15,7 +15,7 @@ from . import Core
 __title__ = 'DecoraterBotCore'
 __author__ = 'Decorater'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2015-2017 Decorater'
+__copyright__ = 'Copyright 2015-2018 AraHaan'
 __version__ = '1.0.0.12'
 __build__ = 0x100000c
 

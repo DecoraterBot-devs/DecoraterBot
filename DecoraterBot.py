5b2c509f9dec33ab2d741a648f0394dcf3cee272
AraHaan <seandhunt_7@yahoo.com>
2022-12-26 10:08:30 -0500

    Update bot's main function to pass in the db readers into BotClient as well as some localized strings from them as well.

--- DecoraterBot.py
+++ DecoraterBot.py
@@ -13,6 +13,7 @@ import sys
 import gc
 import asyncio
 
+from DecoraterBotUtils.readers import DbCredentialsReader, DbLocalizationReader
 from DecoraterBotUtils.client import BotClient
 
 
@@ -20,8 +21,14 @@ async def main():
     """
     EntryPoint to DecoraterBot.
     """
-    async with BotClient() as client:
-        await client.start_bot()
+    async with DbCredentialsReader() as cred_reader:
+        async with DbLocalizationReader() as loc_reader:
+            async with BotClient(
+                    [cred_reader, loc_reader],
+                    description=await loc_reader.get_str_async(5, await cred_reader.language),
+                    activity_name=await loc_reader.get_str_async(3, await cred_reader.language),
+                    activity_url=await loc_reader.get_str_async(6, await cred_reader.language)) as client:
+                await client.start_bot()
 
 
 if __name__ == '__main__':
